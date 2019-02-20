9a64a2eab5eac03539196c184d3a174f9a525412
KevenSaputra <ksaputra@ucsd.edu>
2019-02-19 22:00:40 -0800

    adding control for branch

--- Ctrl.sv
+++ Ctrl.sv
@@ -6,24 +6,20 @@ import definitions::*;
 module Ctrl (
   input[8:0] Instruction,	   // machine code
   input FLAG_IN, 
-  output logic jump_en,
-               branch_en,
+  output logic branch_en,
                flag_write
   );
 // jump on right shift that generates a zero
 always_comb
 begin
-  /*if ((Instruction[8:6] ==  kRSH) && ZERO)
-    jump_en = 1;
+  if ((Instruction[8:6] ==  opOTHER) && FLAG_IN)
+    branch_en = 1;
   else
-    jump_en = 0;*/
+    branch_en = 0;
+
   if (Instruction[8:6] == opCEQ || Instruction[8:6] == opCLT)
     flag_write = 1;
   else 
     flag_write = 0;
 end
 endmodule
-
-   // ARM instructions sequence
-   //				cmp r5, r4
-   //				beq jump_label
