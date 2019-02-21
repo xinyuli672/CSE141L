4f1afbe6fa093447f0d7544b60971206030a7889
XinyuLi <xil672@ucsd.edu>
2019-02-21 15:21:39 -0800

    modified toplevel

--- TopLevel.sv
+++ TopLevel.sv
@@ -24,10 +24,73 @@ wire Overflow_In, Overflow_Out,
 wire  MEM_READ,	   // data_memory read enable
 			MEM_WRITE,	   // data_memory write enable
 			reg_wr_en,	   // reg_file write enable
-			overflow_en,	       // carry reg enable
+      reg_wr_imm_en,  // reg_file write immidiate enable
+			overflow_write,	       // carry reg enable
+      flag_write,      // flag contro
 			branch_en;	   // to program counter: branch enable
 logic[15:0] cycle_ct;	   // standalone; NOT PC!
 logic       SC_IN;         // carry register (loop with ALU) 
+
+// Control decoder
+  Ctrl Ctrl1 (
+	.Instruction,    // from instr_ROM
+	.FLAG_IN,		 // to PC
+	.branch_en,		 // to PC
+  .flag_write,
+  .overflow_write
+  );
+
+// instruction ROM
+  InstROM instr_ROM1(
+	.InstAddress   (PC), 
+	.InstOut       (Instruction)
+	);
+  assign load_inst = Instruction[8:6]==3'b000;  // calls out load specially
+
+
+// reg file
+	reg_file #(.W(8),.D(3)) reg_file1 (
+		.CLK    				  ,
+		.write_en  (reg_wr_en)    ,
+    .write_imm (reg_wr_imm_en), 
+		.raddrA    (Instruction[5:3]), 
+		.raddrB    (Instruction[2:0]), 
+		.waddr     (Instruction[5:3]), 
+		.data_in   (regWriteValue) , 
+		.data_outA (ReadA        ) , 
+		.data_outB (ReadB		 )
+	);
+
+// one pointer, two adjacent read accesses: (optional approach)
+//	.raddrA (Instruction[5:3]);
+//	.raddrB (Instruction[5:3]);
+
+  assign InA = ReadA;						          // connect RF out to ALU in
+	assign InB = ReadB;
+	assign MEM_WRITE = (Instruction[8:6] == 9'b001);       // sw command
+	assign regWriteValue = load_inst? Mem_Out : ALU_out;  // 2:1 switch into reg_file
+    ALU ALU1  (
+	  .INPUTA  (InA),
+	  .INPUTB  (InB), 
+	  .OP      (Instruction[8:6]),
+    .FUNC    (Instruction[2:0]),
+	  .OUT     (ALU_out),//regWriteValue),
+	  .FLAG_IN   ,
+    .OVERFLOW_IN,
+	  .FLAG_OUT,
+    .OVERFLOW_OUT
+	  );
+  
+	data_mem data_mem1(
+		.DataAddress  (ReadA)    , 
+		.ReadMem      (1'b1),          //(MEM_READ) ,   always enabled 
+		.WriteMem     (MEM_WRITE), 
+		.DataIn       (memWriteValue), 
+		.DataOut      (Mem_Out)  , 
+		.CLK 		  		     ,
+		.reset		  (start)
+	);
+	
 	
 // count number of instructions executed
 always_ff @(posedge CLK)
@@ -38,7 +101,7 @@ always_ff @(posedge CLK)
 
 always_ff @(posedge CLK)    // carry/shift in/out register
   if(sc_clr)				// tie sc_clr low if this function not needed
-    SC_IN <= 0;             // clear/reset the carry (optional)
+    FLAG_IN <= 0;             // clear/reset the carry (optional)
   else if(sc_en)			// tie sc_en high if carry always updates on every clock cycle (no holdovers)
     SC_IN <= SC_OUT;        // update the carry  
 
