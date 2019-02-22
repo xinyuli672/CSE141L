0a2b24853d754e939efe6e95a1ea02c29937d7ec
KevenSaputra <ksaputra@ucsd.edu>
2019-02-21 16:31:10 -0800

    removing unnecessary LUT and finishing LUT with placeholder target

--- LUT.sv
+++ LUT.sv
@@ -2,34 +2,28 @@
 // possible lookup table for PC target
 // leverage a few-bit pointer to a wider number
 module LUT(
-  input[2:0] addr,
-  input      Halt,
-  output[2:0] program,
+  input[4:0] addr,
   output logic[9:0] Target
   );
 
-  logic [23:0] target_array[3]
-
-always_ff @(posedge CLK)
-  if (Halt)
-    program <= program + 1;
-
 always_comb begin
   case (addr)		 
-	3'b000: Target = target_array[program][23:21];
-  3'b001: Target = target_array[program][20:18];
-  3'b010: Target = target_array[program][17:15];
-  3'b011: Target = target_array[program][14:12];
-  3'b100: Target = target_array[program][11:9];
-  3'b101: Target = target_array[program][8:6];
-  3'b110: Target = target_array[program][5:3];
-  3'b111: Target = target_array[program][2:0];
+	5'b00000: Target = 10'b0000000000;
+  5'b00001: Target = 10'b0000000000;
+  5'b00010: Target = 10'b0000000000;
+  5'b00011: Target = 10'b0000000000;
+  5'b00100: Target = 10'b0000000000;
+  5'b00101: Target = 10'b0000000000;
+  5'b00110: Target = 10'b0000000000;
+  5'b00111: Target = 10'b0000000000;
+  5'b01000: Target = 10'b0000000000;
+  5'b01001: Target = 10'b0000000000;
+  5'b01010: Target = 10'b0000000000;
+  5'b01011: Target = 10'b0000000000;
+  5'b01100: Target = 10'b0000000000;
+  5'b01101: Target = 10'b0000000000;
+  5'b01111: Target = 10'b0000000000;
+  default: Target = 10'b0000000000;
   endcase
 end
-
-initial begin
-  $readmemb("branch_target.txt",target_array);
-  program = 3'b000;
-end
-
 endmodule
