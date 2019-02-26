b159e59ed02654f727055da9cd269eab3a416cdd
XinyuLi <xil672@ucsd.edu>
2019-02-26 01:57:17 -0800

    modified lab3.py; still need branching

--- Lab3.py
+++ Lab3.py
@@ -5,7 +5,7 @@ print('Running Lab3:')
 
 
 #fileObj = open("filename", "mode") 
-filename = "Assembly3.txt"
+filename = "Assembly1.txt"
 
 read_file = open(filename, "r") 
 
@@ -23,7 +23,7 @@ w_file = open("Machine3.txt", "w")
 #1. open the file
 #2. for each line in the file,
 #3.     split the string by white spaces
-#4.      if the first string == SET then op3 = 0, else op3 = 1
+#4.      if the first string == SEI then op3 = 0, else op3 = 1
 #5.      
 with open(filename, 'r') as f:
   for line in f:
@@ -34,95 +34,104 @@ with open(filename, 'r') as f:
     print instruction
     print str_array
 
-    if instruction == "SET":
-      op3 = "1"
+    if instruction == "SEI": # I-type
+      opcode = "110"
       imm = str_array[1]  #need to reformat without the hashtag
       imm = imm[1:]
-      bin_imm = '{0:08b}'.format(int(imm)) #8 bit immediate
+      bin_imm = '{0:06b}'.format(int(imm)) #6 bit immediate
       #str_array[2] should be the comment
-      return_set = op3 + bin_imm + '\t' + "#" + " " + instruction \
+      return_set = opcode + bin_imm + '\t' + "#" + " " + instruction \
            + " " + "#" + imm 
       w_file.write(return_set + '\n')
-    else:
-      op3 = "0"      
+    else:      
       op1 = str_array[1]
-
-      if instruction == "LB":
+      # R-type
+      if instruction == "LW":
         opcode = "000"
         op2 = str_array[2]
-      elif instruction == "SB":
+      elif instruction == "SW":
         opcode = "001" 
         op2 = str_array[2]
-      elif instruction == "XOR":
+      elif instruction == "ADD":
         opcode = "010"
         op2 = str_array[2]
-      elif instruction == "AND":
+      elif instruction == "SUB":
         opcode = "011"
         op2 = str_array[2]
-      elif instruction == "ADD":
+      elif instruction == "CEQ":
         opcode = "100" 
         op2 = str_array[2]
-      elif instruction == "SLP": 
+      elif instruction == "CLT": 
         opcode = "101"
-        op2 = "000"         # slp only has 1 arg
-      elif instruction == "CMP":
-        opcode = "110"
-        op2 = str_array[2]
-      elif instruction == "BNE":
-        opcode = "111" 
-        op2 = "000"         # bne only has 1 arg
+        op2 = str_array[2] 
+      # O-type
+      elif instruction == "SHIFTL_X":
+        opcode = "111"
+        op2 = "000"
+      elif instruction == "SHIFTL_F":
+        opcode = "111"
+        op2 = "001"
+      elif instruction == "SHIFTL_O":
+        opcode = "111"
+        op2 = "010"
+      elif instruction == "SHIFTR_X":
+        opcode = "111"
+        op2 = "011"
+      elif instruction == "SHIFTR_F":
+        opcode = "111"
+        op2 = "100"
+      elif instruction == "SHIFTR_O":
+        opcode = "111"
+        op2 = "101"
+      elif instruction == "B0":
+        opcode = "111"
+        op2 = "110"
+      elif instruction == "B1":
+        opcode = "111"
+        op2 = "111"
       else:
         opcode = "error: undefined opcode"
         print "error: undefined opcode"
     
-
-      if ((op1 == "$t2" or op1 == "$t3" or op1 == "$t4") 
-            or (op2 == "$t2" or op2 == "$t3" or op2 == "$t4")):
-         level = "1"
-      else:
-         level = "0"
-
+      #7 registers
       print op1
 
-      if (op1 == "$r0,"):
-        reg1 = "00"
-      elif (op1 == "$LFSR,"):
-        reg1 = "01"
-      elif (op1 == "$t0,"):
-        reg1 = "10" 
-      elif (op1 == "$t1,"):
-        reg1 = "11"
-      elif (op1 == "$t2,"):
-        reg1 = "01"
-      elif (op1 == "$t3,"):
-        reg1 = "10"
-      elif (op1 == "$t4,"):
-        reg1 = "11"
+      if (op1 == "$R0,"):
+        reg1 = "000"
+      elif (op1 == "$R1,"):
+        reg1 = "001"
+      elif (op1 == "$R2,"):
+        reg1 = "010" 
+      elif (op1 == "$R3,"):
+        reg1 = "011"
+      elif (op1 == "$R4,"):
+        reg1 = "100"
+      elif (op1 == "$R5,"):
+        reg1 = "101"
+      elif (op1 == "$R6,"):
+        reg1 = "110"
 
-      if (op2 == "$r0"):
-        reg2 = "00"
-      elif (op2 == "$LFSR"):
-        reg2 = "01"
-      elif (op2 == "$t0"):
-        reg2 = "10" 
-      elif (op2 == "$t1"):
-        reg2 = "11"
-      elif (op2 == "$t2"):
-        reg2 = "01"
-      elif (op2 == "$t3"):
-        reg2 = "10"
-      elif (op2 == "$t4"):
-        reg2 = "11"
 
-      if op2 == "000":
-        return_rtype = op3 + opcode + reg1 + reg2 + level \
+      if (op2 == "$R0"):
+        reg2 = "000"
+      elif (op2 == "$R1"):
+        reg2 = "001"
+      elif (op2 == "$R2"):
+        reg2 = "010" 
+      elif (op2 == "$R3"):
+        reg2 = "011"
+      elif (op2 == "$R4"):
+        reg2 = "100"
+      elif (op2 == "$R5"):
+        reg2 = "101"
+      elif (op2 == "$R6"):
+        reg2 = "110"
+      else: 
+        reg2 = op2
+
+      return_rtype = opcode + reg1 + reg2 \
                     + '\t' + "#" + " " + instruction \
                     + " " + op1 
-      else:
-        return_rtype = op3 + opcode + reg1 + reg2 + level \
-                    + '\t' + "#" + " " + instruction \
-                    + " " + op1 + " " + op2
-        
 
       w_file.write(return_rtype + '\n' )
 
